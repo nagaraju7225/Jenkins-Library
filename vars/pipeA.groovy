d7f8cd459044305c4cc1068256a8d23138dd460b
nagaraju7225 <34837887+nagaraju7225@users.noreply.github.com>
2021-01-17 17:08:34 +0530

    Delete pipeA.groovy

--- vars/pipeA.groovy
+++
@@ -1,8 +0,0 @@
-@Library('project-libraries') _
-verifytoJS{
-  Branch = 'master'
-  ScmUrl = 'https://github.com/kyash55/Ansible.git'
-  Credentials = '6a1f806b-f550-469e-8a0d-05e60efc3033'
-  }
-
-    
